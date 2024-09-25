545a9db88967cdb9cc03b34e35fa9bfc3e771d62
Joscha Amani Gaber <joscha.amani.gaber@gmail.com>
2024-09-25 13:16:33 +0200

    Initial Commit

---
+++ AD_Components/AD_ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AD_ComponentsApp.swift
+//  AD_Components
+//
+//  Created by Joscha Amani Gaber on 25.09.24.
+//
+
+import SwiftUI
+
+@main
+struct AD_ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AD_Components/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AD_Components
+//
+//  Created by Joscha Amani Gaber on 25.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
